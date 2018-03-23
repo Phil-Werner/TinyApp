23675eef23fa3b7497a40a7b9994b9033dce363b
Phil Werner <philwerner@shaw.ca>
2018-03-23 02:10:37 +0000

    created login page and register page

--- express_server.js
+++ express_server.js
@@ -13,22 +13,91 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher"
+  }
+}
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  //console.log(users[req.cookies["user_id"]]);
+  let templateVars = { user: users[req.cookies["user_id"]], urls: urlDatabase };
+  // console.log(templateVars);
+  // console.log(users[req.cookies["user_id"]]);
+  // console.log(req.cookies["user_id"]);
+  // console.log(req.cookies);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"]};
+  let templateVars = { user: users[req.cookies["user_id"]]};
   res.render("urls_new");
 });
 
+app.get("/register", (req, res) => {
+
+  res.render("urls_register");
+});
+
+app.post("/register", (req, res) => {
+
+  let randomId = generateRandomString();
+  let newEmail = req.body.email;
+  let password = req.body.password;
+  //console.log('ID:' + randomId + "email:" + email + "password:" + password);
+
+  //console.log(users);
+
+  if (newEmail === "") {
+    res.status(400).send("Your email cannot be blank!");
+    return;
+  }
+
+  if (password === "") {
+    res.status(400).send("Your password cannot be blank!");
+    return;
+  }
+
+  for (elm in users) {
+    if (users[elm]["email"] === newEmail) {
+      res.status(400).send("This email is already registered.  Please try again and have a nice day.")
+      return;
+    }
+      //console.log(users[elm]["email"]);
+      //console.log(newEmail);
+  }
+
+   users[randomId] = {};
+
+   //console.log(users);
+   users[randomId]["id"] = randomId;
+   users[randomId]["email"] = newEmail;
+   users[randomId]["password"] = password;
+
+  //console.log(users);
+  res.cookie('user_id', randomId);
+  // console.log(users);
+  res.redirect('/urls');
+});
+
+app.get("/login", (req, res) => {
+  let templateVars = { user: null };
+  res.render("login", templateVars);
+});
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { username: req.cookies["username"], shortURL: req.params.id, urls:urlDatabase };
+  let templateVars = { user: users[req.cookies["user_id"]], shortURL: req.params.id, urls:urlDatabase };
   res.render("urls_show", templateVars);
 });
 
@@ -62,14 +131,42 @@ app.post("/urls", (req, res) => {
 
 app.post("/login", (req, res) => {
 
-  res.cookie('username', req.body['username']);
+  let loginEmail = req.body['email'];
+  let loginPassword = req.body['password'];
+
+  for (elm in users) {
+
+    if (users[elm]["email"] === loginEmail) {
+
+        if (users[elm]["password"] === loginPassword) {
+          res.cookie('user_id', users[elm]["id"]);
+          res.redirect('/');
+          return;
+        } else {
+          res.status(403).send("Wrong password!  Try again!");
+          return;
+        }
+    }
+  }
+
+  res.status(403).send("No such user! Try again!");
+
+
+
+  //console.log("loginEmail = " + loginEmail);
+  //console.log("loginPassword = " + loginPassword);
+  //console.log(req.body);
+  //res.cookie('user_id', req.body['username']);
   //console.log(req.body['username']);
-  res.redirect('/urls/');
+
+
+  //res.redirect('/urls/');
 });
 
-app.post("/logout", (req, res) => {
+app.get("/logout", (req, res) => {
 
-  res.clearCookie('username');
+
+  res.clearCookie('user_id');
   res.redirect('/urls/');
 });
 
@@ -96,7 +193,7 @@ function generateRandomString() {
   let outputString = "";
 
   for (var x = 0; x < 6; x++) {
-    let index = Math.round(Math.random() * 62);
+    let index = Math.round(Math.random() * 61);
     outputString += arrayOfAlphaNum[index];
   }
 
